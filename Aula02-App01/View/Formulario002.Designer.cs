75c4c7610dff97b5a70e500ecb8d994f9e7609af
guilherme.rcoelho1 <guilherme.rcoelho1@SBC0588951W10-1>
2023-10-25 08:33:34 -0300

    Adicionar arquivos de projeto.

---
+++ Aula02-App01/View/Formulario002.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Aula02_App01.View
+{
+    partial class Formulario002
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tbx_Nome = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            dtp_Nascimento = new DateTimePicker();
+            nud_Ativ01 = new NumericUpDown();
+            nud_Ativ02 = new NumericUpDown();
+            nud_Ativ03 = new NumericUpDown();
+            nud_Ativ04 = new NumericUpDown();
+            label10 = new Label();
+            nud_Media = new NumericUpDown();
+            lbx_Display = new ListBox();
+            btn_Media = new Button();
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ01).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ02).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ03).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ04).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Media).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(155, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(362, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Calculando a Média dos Melhores Alunos";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(47, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(338, 81);
+            label3.Name = "label3";
+            label3.Size = new Size(148, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Data de Nascimento:";
+            // 
+            // tbx_Nome
+            // 
+            tbx_Nome.Location = new Point(49, 104);
+            tbx_Nome.Name = "tbx_Nome";
+            tbx_Nome.Size = new Size(275, 23);
+            tbx_Nome.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(49, 149);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Inserir Notas";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(335, 202);
+            label5.Name = "label5";
+            label5.Size = new Size(85, 20);
+            label5.TabIndex = 6;
+            label5.Text = "Atividade 1";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(335, 223);
+            label6.Name = "label6";
+            label6.Size = new Size(85, 20);
+            label6.TabIndex = 7;
+            label6.Text = "Atividade 2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(335, 245);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 20);
+            label7.TabIndex = 8;
+            label7.Text = "Atividade 3";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(335, 267);
+            label8.Name = "label8";
+            label8.Size = new Size(85, 20);
+            label8.TabIndex = 9;
+            label8.Text = "Atividade 4";
+            // 
+            // dtp_Nascimento
+            // 
+            dtp_Nascimento.CalendarFont = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            dtp_Nascimento.Format = DateTimePickerFormat.Short;
+            dtp_Nascimento.Location = new Point(367, 104);
+            dtp_Nascimento.Name = "dtp_Nascimento";
+            dtp_Nascimento.Size = new Size(85, 23);
+            dtp_Nascimento.TabIndex = 10;
+            // 
+            // nud_Ativ01
+            // 
+            nud_Ativ01.Location = new Point(426, 199);
+            nud_Ativ01.Name = "nud_Ativ01";
+            nud_Ativ01.Size = new Size(47, 23);
+            nud_Ativ01.TabIndex = 11;
+            // 
+            // nud_Ativ02
+            // 
+            nud_Ativ02.Location = new Point(426, 220);
+            nud_Ativ02.Name = "nud_Ativ02";
+            nud_Ativ02.Size = new Size(47, 23);
+            nud_Ativ02.TabIndex = 12;
+            // 
+            // nud_Ativ03
+            // 
+            nud_Ativ03.Location = new Point(426, 242);
+            nud_Ativ03.Name = "nud_Ativ03";
+            nud_Ativ03.Size = new Size(47, 23);
+            nud_Ativ03.TabIndex = 13;
+            // 
+            // nud_Ativ04
+            // 
+            nud_Ativ04.Location = new Point(426, 264);
+            nud_Ativ04.Name = "nud_Ativ04";
+            nud_Ativ04.Size = new Size(47, 23);
+            nud_Ativ04.TabIndex = 14;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(354, 311);
+            label10.Name = "label10";
+            label10.Size = new Size(51, 20);
+            label10.TabIndex = 16;
+            label10.Text = "Média";
+            // 
+            // nud_Media
+            // 
+            nud_Media.Location = new Point(426, 308);
+            nud_Media.Name = "nud_Media";
+            nud_Media.Size = new Size(47, 23);
+            nud_Media.TabIndex = 17;
+            // 
+            // lbx_Display
+            // 
+            lbx_Display.FormattingEnabled = true;
+            lbx_Display.ItemHeight = 15;
+            lbx_Display.Location = new Point(47, 199);
+            lbx_Display.Name = "lbx_Display";
+            lbx_Display.Size = new Size(246, 139);
+            lbx_Display.TabIndex = 18;
+            // 
+            // btn_Media
+            // 
+            btn_Media.Location = new Point(503, 308);
+            btn_Media.Name = "btn_Media";
+            btn_Media.Size = new Size(96, 23);
+            btn_Media.TabIndex = 19;
+            btn_Media.Text = "Calcular Média";
+            btn_Media.UseVisualStyleBackColor = true;
+            btn_Media.Click += btn_Media_Click;
+            // 
+            // Formulario002
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(658, 450);
+            Controls.Add(btn_Media);
+            Controls.Add(lbx_Display);
+            Controls.Add(nud_Media);
+            Controls.Add(label10);
+            Controls.Add(nud_Ativ04);
+            Controls.Add(nud_Ativ03);
+            Controls.Add(nud_Ativ02);
+            Controls.Add(nud_Ativ01);
+            Controls.Add(dtp_Nascimento);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(tbx_Nome);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Formulario002";
+            ShowIcon = false;
+            Text = "Sistema de Média de Alunos";
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ01).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ02).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ03).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Ativ04).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nud_Media).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbx_Nome;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private DateTimePicker dtp_Nascimento;
+        private NumericUpDown nud_Ativ01;
+        private NumericUpDown nud_Ativ02;
+        private NumericUpDown nud_Ativ03;
+        private NumericUpDown nud_Ativ04;
+        private Label label9;
+        private Label label10;
+        private NumericUpDown nud_Media;
+        private ListBox lbx_Display;
+        private Button btn_Media;
+    }
+}
---
+++ Aula02-App01/View/Formulario002.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula02_App01.View
+{
+    public partial class Formulario002 : Form
+    {
+        public Formulario002()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_Media_Click(object sender, EventArgs e)
+        {
+            string Nome = tbx_Nome.Text;
+            string DataNasc = dtp_Nascimento.Value.ToString("dd/MM/yyyy");
+            string Ativ1 = nud_Ativ01.Value.ToString();
+            string Ativ2 = nud_Ativ02.Value.ToString();
+            string Ativ3 = nud_Ativ03.Value.ToString();
+            string Ativ4 = nud_Ativ04.Value.ToString();
+            string resultado;
+
+            decimal media = 0;
+
+            media = (nud_Ativ01.Value + nud_Ativ02.Value + nud_Ativ03.Value + nud_Ativ04.Value) / 4;
+
+            if (media > nud_Media.Value)
+            {
+                resultado = "Aprovado";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+
+
+            lbx_Display.Items.Add("Nome: " + Nome);
+            lbx_Display.Items.Add("Data de Nascimento: " + DataNasc);
+            lbx_Display.Items.Add("Nota Atividade 1: " + Ativ1);
+            lbx_Display.Items.Add("Nota Atividade 2: " + Ativ2);
+            lbx_Display.Items.Add("Nota Atividade 3: " + Ativ3);
+            lbx_Display.Items.Add("Nota Atividade 4: " + Ativ4);
+            lbx_Display.Items.Add("Média: " + media);
+
+        }
+    }
+}
